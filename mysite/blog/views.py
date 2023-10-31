d675f9f2a220722818a69b127680cb4b81eb0796
Tappollus <appollo1018@gmail.com>
2023-10-31 19:48:03 +0100

    toevoegen les 6 en huiswerk

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):  
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):   
+    post = get_object_or_404(Post, pk=pk)   
+    return render(request, 'blog/post_inhoud.html', {'post': post})
+
+def post_nieuw(request):   
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_nieuw(request):   
+    if request.method == "POST":
+       form = PostForm(request.POST)
+       if form.is_valid():
+           post = form.save(commit=False)
+           post.author = request.user
+           post.published_date = timezone.now()
+           post.save()
+           return redirect('post_inhoud', pk=post.pk)
+    else:
+       form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
